f1bbf9f4262d6bac75d730a52a24690401c9152d
marc7hedev <driberstagg@gmail.com>
2022-11-19 00:21:43 -0600

    First commit: Reproductor de música Musictron

---
+++ assets/js/script.js
@@ -0,0 +1,228 @@
+let songs = [
+    {
+        name: 'Bohemian Rhapsody',
+        src: 'assets/songs/song1.mp3',
+        cover: 'assets/images/song1.jpg',
+        artist: 'Queen',
+        lyrics: "6 | Mamaaa 9\n | Just killed a man\n 13 | Put a gun against his head, pulled my trigger\n17 | Now he's dead\n19.5 | Mamaaa, life had just begun,\n26.4 | But now I've gone and thrown it all away\n33 | Mama, oooh,\n39.5 | Didn't mean to make you cry,\n43 | If I'm not back again this time tomorrow,\n47.8 | Carry on, carry on as if nothing really matters \n64 | Too late, my time has come,\n71 | Sends shivers down my spine \n74 |  body's aching all The time \n78 |  Goodbye, everybody\n81 |  I've got to go \n84.7 |  Gotta leave you all behind and face the truth\n91.7 |  Mama, oooh\n98 | I don't want to die\n101 | I sometimes wish I'd never been born at all",
+    },
+    {
+        name: 'The Phantom Of The Opera',
+        src: 'assets/songs/song2.mp3',
+        cover: 'assets/images/song2.jpg',
+        artist: 'Nightwish',
+        lyrics: '',
+    },
+    {
+        name: 'To the Helfire',
+        src: 'assets/songs/song3.mp3',
+        cover: 'assets/images/song3.jpg',
+        artist: 'Lorna Shore',
+        lyrics: '',
+    },
+    {
+        name: 'Hangar 18',
+        src: 'assets/songs/song4.mp3',
+        cover: 'assets/images/song4.jpg',
+        artist: 'Megadeth',
+        lyrics: '',
+    },
+    {
+        name: 'High(deat.Aleesia)',
+        src: 'assets/songs/song10.mp3',
+        cover: 'assets/images/song10.jpg',
+        artist: 'JPB',
+        lyrics: '',
+    },
+    {
+        name: 'Long Night pt.||',
+        src: 'assets/songs/song11.mp3',
+        cover: 'assets/images/song11.jpg',
+        artist: 'JPB',
+        lyrics:
+            '2.8 | We gon turn the crib into a club like\n' +
+            "5.7 | Hennessy and Buddah screamin' thug life'\n" +
+            " 8 | Get it baby, you know I'm the plug, right'\n" +
+            " 10 | Buggin' got a feelin' that it's gonna be a long night'\n" +
+            " 11 | Whoa'\n" +
+            " 13 | Everything I touch turned gold'\n" +
+            " 15 | Need no introduction, 'cause they know briggin more''\n" +
+            " 17.3 | I hit the lotto, my motto roll up Gelato and Throttle'\n" +
+            " 20 | A couple models to caudal who fuckin' brought out the bottles'\n" +
+            " 22 | I'm goin' ham'\n" +
+            " 23 | This ain't even luck, all planned'\n" +
+            " 25 | Y'all know who the fucks I am'\n" +
+            " 27 | Burnin' bucks mo bands'\n" +
+            " 29 | Always been the M.O. since the start of this'\n" +
+            " 30.5 | And now I'm on a mission like I'm Optimus, aye'\n" +
+            "  33.5| We gon turn the crib into a club like'\n" +
+            "  36 | Hennessy and Buddah screamin' thug life'\n" +
+            "  39 | Get it baby, you know I'm the plug, right'\n" +
+            "  40 | Buggin' got a feelin' that it's gonna be a long night'\n" +
+            "  51 | Got a feelin' that it's gonna be a long night'\n" +
+            "  62 | Yеah, never an averagе night'\n" +
+            "  64 | Two, four hours up in a day I gotta make it right'\n" +
+            "  67 | 18 of those I'm in a cave I gotta make some light'\n" +
+            "  69 | Make it hype, even if we breakin' night'\n" +
+            "72 | And even if things go left we gotta make 'em right'\n" +
+            "74 |  Chillin' with M.I.M.E we triple up aye 'onna double UP'\n" +
+            "76.4 | Triple dub, came from the basement we only goin' up'\n" +
+            "79 | They lovin' us, up in the booth I'm getting dizzy,'\n" +
+            "80  | but aye, the cup is up'\n" +
+            "82  | Toast to my people, that's on the come up, aye'\n" +
+            "84  | You lit with us, in order by the trigger buzz'\n" +
+            "86  | Lookin' at my watches a spirit bomb both hands are goin' up'\n" +
+            "88.5  | You comin' coast to coast with us'\n" +
+            "90  | See the flows are flowin' over, flowin' yeah'\n" +
+            "92  | The cup it run it over title wave up in the ocean,ah'\n" +
+            "95  | We gon turn the crib into a club like'\n" +
+            "97  | Hennessy and Buddah screamin' thug life'\n" +
+            "100  | Get it baby, you know I'm the plug, right'\n" +
+            "102  | Buggin' got a feelin' that it's gonna be a long night'\n" +
+            "112  | Got a feelin' that it's gonna be a long night'\n",
+    },
+    {
+        name: 'Royalty(ft.Neoni)',
+        src: 'assets/songs/song9.mp3',
+        cover: 'assets/images/song9.jpg',
+        artist: 'Egzod & Maestro Chives ',
+        lyrics:
+            "11.5 | I've been cold hearted\n" +
+            "14 | But I'm just getting started\n" +
+            '18 | Got my eyes on the the targetNow\n' +
+            '22 | now\n' +
+            "32 | 'Til the battles are won\n" +
+            "35 | 'Til kingdom come\n" +
+            "45 | I'll never run \n" +
+            '48 | Best to give me your loyalty\n' +
+            "52 | 'Cause I'm taking the world, you'll see\n" +
+            "55 | They'll be calling me, calling me\n" +
+            "58 | They'll be calling me royalty\n" +
+            '60 | Best to give me your loyalty\n' +
+            "64 | 'Cause I'm taking the world, you'll see\n" +
+            "67 | They'll be calling me, calling me\n" +
+            "70 | They'll be calling me royalty\n" +
+            "82 | They'll be calling me royalty\n",
+    },
+    {
+        name: 'Moonlight Sonata',
+        src: 'assets/songs/song8.mp3',
+        cover: 'assets/images/song8.jpg',
+        artist: 'Dr.Viossy',
+        lyrics: '',
+    },
+    {
+        name: '512',
+        src: 'assets/songs/song5.mp3',
+        cover: 'assets/images/song5.jpg',
+        artist: 'Lamb of God',
+        lyrics: '',
+    },
+    {
+        name: 'Tommy T',
+        src: 'assets/songs/song6.mp3',
+        cover: 'assets/images/song6.jpg',
+        artist: 'Wiked x Tommy T',
+        lyrics: '',
+    },
+    {
+        name: "You're Nobody",
+        src: 'assets/songs/song7.mp3',
+        cover: 'assets/images/song7.jpg',
+        artist: 'Biggie Smalls',
+        lyrics: '',
+    },
+];
+let artists = [
+    {
+        name: 'Queen',
+        picture: 'assets/images/freddie.png',
+    },
+    {
+        name: 'Michael Jackson',
+        picture: 'assets/images/mich.jpg',
+    },
+    {
+        name: 'Led Zeppelin',
+        picture: 'assets/images/robert.jpg',
+    },
+    {
+        name: 'Kendrick Lamar',
+        picture: 'assets/images/kendrick.jpg',
+    },
+    {
+        name: 'Ghost',
+        picture: 'assets/images/ghost.jpg',
+    },
+    {
+        name: 'Cradle of filth',
+        picture: 'assets/images/cradle.jpeg',
+    },
+    {
+        name: 'Rammstein',
+        picture: 'assets/images/ramm.jpg',
+    },
+    {
+        name: 'Judas Priest',
+        picture: 'assets/images/judas.jpg',
+    },
+    {
+        name: 'Mac Miller',
+        picture: 'assets/images/mac.jpg',
+    },
+];
+let albums = [
+    {
+        name: 'Master of puppets',
+        picture: 'assets/images/master.jfif',
+    },
+    {
+        name: 'Rust in peace',
+        picture: 'assets/images/megadeth.jfif',
+    },
+    {
+        name: 'Ready to die',
+        picture: 'assets/images/rdie.jfif',
+    },
+    {
+        name: 'Automatic for the People',
+        picture: 'assets/images/autm.jfif',
+    },
+    {
+        name: 'Vaughan Texas Flood',
+        picture: 'assets/images/vaughan.jfif',
+    },
+    {
+        name: 'Korn (1994)',
+        picture: 'assets/images/korn.jpg',
+    },
+    {
+        name: 'Illmatic',
+        picture: 'assets/images/Illmatic.jfif',
+    },
+    {
+        name: 'King 810',
+        picture: 'assets/images/king.jpg',
+    },
+];
+function numberformatter(num){
+    return num >= 10 ? num.toString() : "0" + num.toString();
+}
+
+$(document).ready(function(){
+    songs.map((song, i) => {
+        let template = 
+            '<div class="song" data-id="'+
+            i +
+            '"> <img src="assets/images/equalizer.gif" class="equalizer_gif" alt="" /> <div class="song_num">'+
+            numberformatter(i + 1)+
+            '</div> <div style="position: relative;"> <img src="'+
+            song.cover+
+            '" class="song_img" alt="" /> <img src="assets/images/play.png" alt="" class="play_hover" /> </div> <div class="song_info"> <div class="song_name">'+
+            song.name+
+            '</div> <div class="song_artist">'+
+            song.artist+
+            '</div> </div> </div>';
+        $(".left_songs_list").append(template);
+    });
+});
